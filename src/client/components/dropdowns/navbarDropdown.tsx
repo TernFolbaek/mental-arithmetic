e0df207c753f84e8362da982721a74355f9cbbbb
Tern Folbaek <tfolbaek@gmail.com>
2023-07-12 23:15:20 +0200

    fix dropdown overlap with button

--- src/client/components/dropdowns/navbarDropdown.tsx
+++ src/client/components/dropdowns/navbarDropdown.tsx
@@ -40,7 +40,7 @@ export const Dropdown: React.FC<DropdownProps> = ({ options, onOptionSelected })
 
   return (
     <div className="relative w-full">
-      <div className="relative pr-1 pl-1 w-full rounded flex justify-center" ref={dropdownRef} >
+      <div className="pr-1 pl-1 w-full rounded flex justify-center" ref={dropdownRef} >
         <button onClick={(e) => setIsOpen(!isOpen)} className="focus:outline-none text-[25px] text-white flex items-center">
           <span className="hidden inline-block">{selectedOption}</span>
           <Icon
@@ -50,7 +50,7 @@ export const Dropdown: React.FC<DropdownProps> = ({ options, onOptionSelected })
           />
         </button>
         {isOpen && (
-          <div className="absolute left-0 mt-2 w-full sm:w-48 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 overflow-auto max-h-screen">
+          <div className="absolute top-full mt-2 w-full sm:w-48 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 overflow-auto max-h-screen">
             <div className="py-1 rounded-md bg-white shadow-xs" role="menu" aria-orientation="vertical" aria-labelledby="options-menu">
               {options.map((option, index) => (
                 <a key={index} onClick={() => selectOption(option)} className="block px-4 py-2 text-sm text-gray cursor-pointer hover:bg-gray-100" role="menuitem">{option}</a>
